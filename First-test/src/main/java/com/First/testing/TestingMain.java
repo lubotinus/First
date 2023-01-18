0e87ea7ddb1e668157daec8a93e3373d45180b03
lubotinus <barankov.dmitry@gmail.com>
2023-01-18 23:11:59 +0100

    dgsd

--- First-test/src/main/java/com/First/testing/TestingMain.java
+++ First-test/src/main/java/com/First/testing/TestingMain.java
@@ -1,2 +1,7 @@
-package com.First.testing;public class TestingMain {
+package com.First.testing;
+
+public class TestingMain {
+    public static void main(String[] args) {
+        System.out.println(" Moduł 6. Wprowadzenie do testowania opragramowania");
+    }
 }
